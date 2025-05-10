7f20c5bd4bfe2316d05064bb7c11d18b501afb89
Matsuura Yudai <myudai0306@gmail.com>
2025-05-10 21:16:31 +0900

    change to all joint

--- README.md
+++ README.md
@@ -2,8 +2,4 @@
 This repository analyzes ros2bag data obtained from ros.
 
 ## install
-pandas // to process csv file
-pip install pandas
-
-matplotlib // to visualize 
-pip install matplotlib
+pip install pandas matplotlib
--- src/rosbag_visualizer.py
+++ src/rosbag_visualizer.py
@@ -3,13 +3,34 @@ import matplotlib.pyplot as plt
 
 csv_file = "../data/csv/take6_joint_states.csv"
 df = pd.read_csv(csv_file)
-df_clean = df[['__time', '/LF/joint_states/LF_B2C/effort']].dropna() ## remove NaN
 
-# print(df.columns)
-df_clean['relative_time'] = df_clean['__time'] - df_clean['__time'].iloc[0]
+rename_dict = {
+    '__time': 'time',
+    '/LF/joint_states/LF_B2C/effort': 'B2C',
+    '/LF/joint_states/LF_C2F/effort': 'C2F',
+    '/LF/joint_states/LF_F2T/effort': 'F2T',
+    '/LF/joint_states/LF_T2E/effort': 'T2E',
+    '/LF/joint_states/LF_steering/effort': 'steering',
+    '/LF/joint_states/LF_driving/effort': 'driving',
+    '/LF/joint_states/LF_gripper/effort': 'gripper'
+}
+
+df_clean = df[list(rename_dict.keys())].rename(columns = rename_dict).dropna() ## remove NaN
+
+# print(df.columns) ## for debug
+df_clean['relative_time'] = df_clean['time'] - df_clean['time'].iloc[0]
+
+# plot figure
 plt.figure(figsize = (10, 6))
-plt.plot(df_clean['relative_time'], df_clean['/LF/joint_states/LF_B2C/effort'], label = 'LF_B2C')
+plt.plot(df_clean['relative_time'], df_clean['B2C'], label = 'LF_B2C', color = 'red')
+plt.plot(df_clean['relative_time'], df_clean['C2F'], label = 'LF_C2F', color = 'blue')
+plt.plot(df_clean['relative_time'], df_clean['F2T'], label = 'LF_F2T', color = 'green')
+plt.plot(df_clean['relative_time'], df_clean['T2E'], label = 'LF_T2E', color = 'orange')
+plt.plot(df_clean['relative_time'], df_clean['steering'], label = 'LF_steering', color = 'yellow')
+plt.plot(df_clean['relative_time'], df_clean['driving'], label = 'LF_driving', color = 'cyan')
+plt.plot(df_clean['relative_time'], df_clean['gripper'], label = 'LF_gripper', color = 'magenta')
 
+# figure config
 plt.xlabel('Time [s]')
 plt.ylabel('Effort')
 plt.legend()
