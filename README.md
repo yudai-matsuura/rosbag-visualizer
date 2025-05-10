d4b40da6950b52a81fe612ea7e3605a67c83b14c
Matsuura Yudai <myudai0306@gmail.com>
2025-05-10 23:11:37 +0900

    made function and change code to save png

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ This repository analyzes ros2bag data obtained from ros.
 
 ## install
 pip install pandas matplotlib
+
+## execute
+cd src
+python3 rosbag_visualizer.py
--- src/rosbag_visualizer.py
+++ src/rosbag_visualizer.py
@@ -7,81 +7,33 @@ from include import rosbag_visualizer_config
 
 csv_file = "../data/csv/take6_joint_states.csv"
 df = pd.read_csv(csv_file)
+result_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
+os.makedirs(result_dir, exist_ok = True)
 
-### LF
-df_lf = df[list(rosbag_visualizer_config.LF_rename_dict.keys())].rename(columns = rosbag_visualizer_config.LF_rename_dict).dropna() ## remove NaN
-df_lf['relative_time'] = df_lf['time'] - df_lf['time'].iloc[0]
-# plot figure
-plt.figure(figsize = (10, 6))
-plt.plot(df_lf['relative_time'], df_lf['B2C'], label = 'LF_B2C', color = 'red')
-plt.plot(df_lf['relative_time'], df_lf['C2F'], label = 'LF_C2F', color = 'blue')
-plt.plot(df_lf['relative_time'], df_lf['F2T'], label = 'LF_F2T', color = 'green')
-plt.plot(df_lf['relative_time'], df_lf['T2E'], label = 'LF_T2E', color = 'orange')
-plt.plot(df_lf['relative_time'], df_lf['steering'], label = 'LF_steering', color = 'yellow')
-plt.plot(df_lf['relative_time'], df_lf['driving'], label = 'LF_driving', color = 'cyan')
-plt.plot(df_lf['relative_time'], df_lf['gripper'], label = 'LF_gripper', color = 'magenta')
-# figure config
-plt.xlabel('Time [s]')
-plt.ylabel('Effort')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+def plot_leg_effort(df, rename_dict, leg_label):
+    df_leg = df[list(rename_dict.keys())].rename(columns = rename_dict).dropna()
+    df_leg['relative_time'] = df_leg['time'] - df_leg['time'].iloc[0]
 
-### LH
-df_lh = df[list(rosbag_visualizer_config.LH_rename_dict.keys())].rename(columns = rosbag_visualizer_config.LH_rename_dict).dropna() ## remove NaN
-df_lh['relative_time'] = df_lh['time'] - df_lh['time'].iloc[0]
-# plot figure
-plt.figure(figsize = (10, 6))
-plt.plot(df_lh['relative_time'], df_lh['B2C'], label = 'LH_B2C', color = 'red')
-plt.plot(df_lh['relative_time'], df_lh['C2F'], label = 'LH_C2F', color = 'blue')
-plt.plot(df_lh['relative_time'], df_lh['F2T'], label = 'LH_F2T', color = 'green')
-plt.plot(df_lh['relative_time'], df_lh['T2E'], label = 'LH_T2E', color = 'orange')
-plt.plot(df_lh['relative_time'], df_lh['steering'], label = 'LH_steering', color = 'yellow')
-plt.plot(df_lh['relative_time'], df_lh['driving'], label = 'LH_driving', color = 'cyan')
-plt.plot(df_lh['relative_time'], df_lh['gripper'], label = 'LH_gripper', color = 'magenta')
-# figure config
-plt.xlabel('Time [s]')
-plt.ylabel('Effort')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+    plt.figure(figsize=(10, 6))
+    plt.plot(df_leg['relative_time'], df_leg['B2C'], label=f'{leg_label}_B2C', color='red')
+    plt.plot(df_leg['relative_time'], df_leg['C2F'], label=f'{leg_label}_C2F', color='blue')
+    plt.plot(df_leg['relative_time'], df_leg['F2T'], label=f'{leg_label}_F2T', color='green')
+    plt.plot(df_leg['relative_time'], df_leg['T2E'], label=f'{leg_label}_T2E', color='orange')
+    plt.plot(df_leg['relative_time'], df_leg['steering'], label=f'{leg_label}_steering', color='yellow')
+    plt.plot(df_leg['relative_time'], df_leg['driving'], label=f'{leg_label}_driving', color='cyan')
+    plt.plot(df_leg['relative_time'], df_leg['gripper'], label=f'{leg_label}_gripper', color='magenta')
+    plt.xlabel('Time [s]')
+    plt.ylabel('Effort [mA]')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
 
-### RH
-df_rh = df[list(rosbag_visualizer_config.RH_rename_dict.keys())].rename(columns = rosbag_visualizer_config.RH_rename_dict).dropna() ## remove NaN
-df_rh['relative_time'] = df_rh['time'] - df_rh['time'].iloc[0]
-# plot figure
-plt.figure(figsize = (10, 6))
-plt.plot(df_rh['relative_time'], df_rh['B2C'], label = 'RH_B2C', color = 'red')
-plt.plot(df_rh['relative_time'], df_rh['C2F'], label = 'RH_C2F', color = 'blue')
-plt.plot(df_rh['relative_time'], df_rh['F2T'], label = 'RH_F2T', color = 'green')
-plt.plot(df_rh['relative_time'], df_rh['T2E'], label = 'RH_T2E', color = 'orange')
-plt.plot(df_rh['relative_time'], df_rh['steering'], label = 'RH_steering', color = 'yellow')
-plt.plot(df_rh['relative_time'], df_rh['driving'], label = 'RH_driving', color = 'cyan')
-plt.plot(df_rh['relative_time'], df_rh['gripper'], label = 'RH_gripper', color = 'magenta')
-# figure config
-plt.xlabel('Time [s]')
-plt.ylabel('Effort [mA]')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+    save_path = os.path.join(result_dir, f'{leg_label}_effort.png')
+    plt.savefig(save_path)
 
-### RF
-df_rf = df[list(rosbag_visualizer_config.RF_rename_dict.keys())].rename(columns = rosbag_visualizer_config.RF_rename_dict).dropna() ## remove NaN
-df_rf['relative_time'] = df_rf['time'] - df_rf['time'].iloc[0]
-# plot figure
-plt.figure(figsize = (10, 6))
-plt.plot(df_rf['relative_time'], df_rf['B2C'], label = 'RF_B2C', color = 'red')
-plt.plot(df_rf['relative_time'], df_rf['C2F'], label = 'RF_C2F', color = 'blue')
-plt.plot(df_rf['relative_time'], df_rf['F2T'], label = 'RF_F2T', color = 'green')
-plt.plot(df_rf['relative_time'], df_rf['T2E'], label = 'RF_T2E', color = 'orange')
-plt.plot(df_rf['relative_time'], df_rf['steering'], label = 'RF_steering', color = 'yellow')
-plt.plot(df_rf['relative_time'], df_rf['driving'], label = 'RF_driving', color = 'cyan')
-plt.plot(df_rf['relative_time'], df_rf['gripper'], label = 'RF_gripper', color = 'magenta')
-# figure config
-plt.xlabel('Time [s]')
-plt.ylabel('Effort')
-plt.legend()
-plt.grid(True)
-plt.tight_layout()
+plot_leg_effort(df, rosbag_visualizer_config.LF_rename_dict, 'LF')
+plot_leg_effort(df, rosbag_visualizer_config.LH_rename_dict, 'LH')
+plot_leg_effort(df, rosbag_visualizer_config.RH_rename_dict, 'RH')
+plot_leg_effort(df, rosbag_visualizer_config.RF_rename_dict, 'RF')
 
 plt.show()
