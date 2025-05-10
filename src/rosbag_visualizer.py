5a299130d1da59e250e48babb1ff67d4d1581477
Matsuura Yudai <myudai0306@gmail.com>
2025-05-10 17:30:34 +0900

    small change

--- src/rosbag_visualizer.py
+++ src/rosbag_visualizer.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 csv_file = "../data/csv/take6_joint_states.csv"
 df = pd.read_csv(csv_file)
+df_clean = df[['__time', '/LF/joint_states/LF_B2C/effort']].dropna() ## remove NaN
 
-print(df.columns)
-
+# print(df.columns)
+df_clean['relative_time'] = df_clean['__time'] - df_clean['__time'].iloc[0]
 plt.figure(figsize = (10, 6))
-plt.plot(df['_time'], df['/LF/joint_states/LF_B2C/effort'], label = 'LF_B2C')
+plt.plot(df_clean['relative_time'], df_clean['/LF/joint_states/LF_B2C/effort'], label = 'LF_B2C')
 
 plt.xlabel('Time [s]')
 plt.ylabel('Effort')
